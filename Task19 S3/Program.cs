3df84d6c93f3bc610fa90ad49c23916802839508
Dmitrii <petrovoo096@gmail.com>
2022-12-13 18:34:59 +0300

    Task19

---
+++ Task19 S3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.WriteLine("Введите пятизначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int temp = a;
+int remainder, revNumber = 0;
+while (a > 0)
+{
+    remainder = a % 10;
+    a = a / 10;
+    revNumber = (revNumber * 10) + remainder;
+}
+if (temp == revNumber)
+{
+    Console.WriteLine("Это число палиндром");
+}
+else
+{
+    Console.WriteLine("НЕ палиндром");
+}
