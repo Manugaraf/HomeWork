61cb85295f2acc4bdcced5576e469fc911d63292
Dmitrii <petrovoo096@gmail.com>
2022-12-23 19:44:39 +0300

    Task41

--- Task41 Seminar6/Program.cs
+++ Task41 Seminar6/Program.cs
@@ -2,4 +2,26 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 4
 
+int count = 0;
+int[] array;
+array = new int[10000];
+Console.WriteLine("Введите числа массива");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
 
+    if (array[i] > 0)
+    {
+        while (Console.ReadLine() != "stop")
+        {
+            count++;
+        }
+        break;
+    }
+
+    // else if (array[i] == "stop")
+    // {
+    //     Console.WriteLine($"Чисел больше нуля: {count}");
+    // }
+}
+Console.WriteLine($"Чисел больше нуля: {count}");
